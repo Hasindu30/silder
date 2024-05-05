9264f2c8654be7d81a63a5a0a7db011ab1d43020
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-05-06 00:51:18 +0530

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/assets/slider1.png 	new file:   src/assets/slider2.jpeg 	new file:   src/assets/slider3.jpeg 	new file:   src/assets/slider4.jpg 	new file:   src/components/custom.slider.css 	new file:   src/components/custom.slider.jsx 	new file:   src/data/images.jsx 	modified:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import images from "./data/images";
+
+import CustomSlider from "./components/custom.slider";
+
+import './index.css';
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomSlider>
+        {images.map((image, index) => {
+          return <img key={index} src={image.imgURL} alt={image.imgAlt} />;
+        })}
+      </CustomSlider>
+      
     </div>
   );
 }
-
-export default App;
