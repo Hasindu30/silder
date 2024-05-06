bcfd269b23a028e30454482a8b6b98af0b4e3aab
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-05-06 11:31:03 +0530

    new file:   src/assets/slider1.jpeg 	deleted:    src/assets/slider1.png 	modified:   src/assets/slider2.jpeg 	modified:   src/assets/slider3.jpeg 	new file:   src/assets/slider4.jpeg 	deleted:    src/assets/slider4.jpg 	modified:   src/components/custom.slider.css 	modified:   src/data/images.jsx

--- src/data/images.jsx
+++ src/data/images.jsx
@@ -1,7 +1,7 @@
-import slider1 from '../assets/slider1.png';
+import slider1 from '../assets/slider1.jpeg';
 import slider2 from '../assets/slider2.jpeg';
 import slider3 from '../assets/slider3.jpeg';
-import slider4 from '../assets/slider4.jpg';
+import slider4 from '../assets/slider4.jpeg';
 
 
 const images = [
